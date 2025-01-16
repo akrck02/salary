2c69ec8e59c6137def7fe05ff41e7695778f0d77
akrck02 <aketza.vazquez@gmail.com>
2025-01-16 23:43:56 +0100

    merge: fix

--- src/services/language.ts
+++ src/services/language.ts
@@ -2,19 +2,16 @@ import { HomeTexts, TextBundles } from "../core/texts.js";
 import { getText, loadTextBundle } from "../lib/i18n.js";
 import { connectToSignal, setSignal } from "../lib/signals.js";
 
-export const reloadTextSignal = setSignal()
-connectToSignal(reloadTextSignal, reloadText)
-
+export const reloadTextSignal = setSignal();
+connectToSignal(reloadTextSignal, reloadText);
 
 async function reloadText() {
-  document.title = `${getText(TextBundles.Home, HomeTexts.AppName)} - ${getText(TextBundles.Home, HomeTexts.Title)}`
-  
-  const elements = document.querySelectorAll("*[data-i18n]")
+  document.title = `${getText(TextBundles.Home, HomeTexts.AppName)} - ${getText(TextBundles.Home, HomeTexts.Title)}`;
+
+  const elements = document.querySelectorAll("*[data-i18n]");
 
   for (const element of elements) {
-    const parts = element.getAttribute("data-i18n").split(":")
-    console.log(parts);
-    element.textContent = getText(parts[0], parts[1])
+    const parts = element.getAttribute("data-i18n").split(":");
+    element.textContent = getText(parts[0], parts[1]);
   }
-
 }
