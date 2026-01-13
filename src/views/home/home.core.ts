e01a1de40d995249f2663fbb02263c74f8a6c8bb
Roger <123881876+RayxnorDev@users.noreply.github.com>
2026-01-13 17:30:56 +0100

    Updated IRPF 2026 values due to the Araba Foral Decree 21/2025, of December 29, which modify IRPF coming into force from January 1 (2026) onwards Updated 2026 value for MEI (0,13% to 0,15%) included into CC (4,83% to 4,85%)

--- src/views/home/home.core.ts
+++ src/views/home/home.core.ts
@@ -1,6 +1,6 @@
 // ---------- constants ------------
 export const AVAILABLE_REGIONS = ["paisvasco"]
-export const AVAILABLE_YEARS = [2025, 2024, 2023, 2022]
+export const AVAILABLE_YEARS = [2026, 2025, 2024, 2023, 2022]
 export const AVAILABLE_PAYMENT_NUMBERS = [14, 12]
 export const MAX_SALARY = 1000000
 export const MIN_SALARY = 0
